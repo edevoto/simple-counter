a180b3f493682c985b59a62e08350ad9c73b4adc
enzo devoto <enzodevotob@gmail.com>
2021-06-08 17:54:47 -0400

    Contador terminado.

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,27 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-export function Home() {
+export function Counter(props) {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex bg-dark justify-content-center display-4 text-white align-content-center">
+			<div className="calendar">
+				<i className="far fa-clock"></i>
+			</div>
+			<div className="fourth">{props.fourthDigit % 10}</div>
+			<div className="third">{props.thirdDigit % 10}</div>
+			<p>:</p>
+			<div className="second">{props.secondDigit % 10}</div>
+			<div className="first">{props.firstDigit % 10}</div>
 		</div>
 	);
 }
+
+Counter.propTypes = {
+	fourthDigit: PropTypes.number,
+	thirdDigit: PropTypes.number,
+	secondDigit: PropTypes.number,
+	firstDigit: PropTypes.number
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
+import PropTypes from "prop-types";
 //include bootstrap npm library into the bundle
 import "bootstrap";
 
@@ -9,7 +9,24 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+import { Counter } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(function() {
+	const four = Math.floor(counter / 1000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+	counter++;
+
+	ReactDOM.render(
+		<Counter
+			firstDigit={one}
+			secondDigit={two}
+			thirdDigit={three}
+			fourthDigit={four}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
